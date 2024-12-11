56cbae17efed083f392c10c943661d7bfcf9bda9
Thean iT <Theanit@gmail.com>
2024-12-11 10:15:52 +0700

    gg

--- upl.php
+++ upl.php
@@ -1,23 +1,20 @@
 <?php
-    $photo = $_FILES['txt-file'];
-    $photoName =  $photo['name'];
-    $photoSize = $photo['size'];
-    $imgName = mt_rand(100000,999999);
-    $tmp = $photo['tmp_name'];
-    // echo $photoName;
-    // alert("123");
-    $ext = pathinfo($photoName, PATHINFO_EXTENSION);
-    // echo '<br>';
-    // echo $ext;
-    // echo '<br>';
-    // echo time();
-    // move_uploaded_file($tmp,'img/'.time().$imgName.
-    // '.'.$ext);
+$photo = $_FILES['txt-file'];
+$photoName =  $photo['name'];
+$photoSize = $photo['size'];
+$imgName = mt_rand(100000, 999999);
+$tmp = $photo['tmp_name'];
+// echo $photoName;
+// alert("123");
+$ext = pathinfo($photoName, PATHINFO_EXTENSION);
+// echo '<br>';
+// echo $ext;
+// echo '<br>';
+// echo time();
+// move_uploaded_file($tmp,'img/'.time().$imgName.
+// '.'.$ext);
 
-    $newName = time().$imgName.'.'.$ext;
-    move_uploaded_file($tmp,'img/'.$newName);
-    $msg['imgName'] = $newName;
-    echo json_encode($msg);
-
-
-?>
+$newName = time() . $imgName . '.' . $ext;
+move_uploaded_file($tmp, 'img/' . $newName);
+$msg['imgName'] = $newName;
+echo json_encode($msg);
